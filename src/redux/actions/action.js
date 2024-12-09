2ef8e0fee4dc59cc462b457476c3fb8274942d55
frontend <frontend.divine@gmail.com>
2024-12-09 12:40:03 +0530

    change url

--- src/redux/actions/action.js
+++ src/redux/actions/action.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+const baseURL = "https://templateshub-backend.onrender.com";
 console.log("baseURL :>> ", baseURL);
 
 export const post = (url, data, actionType, dispatch) => {
